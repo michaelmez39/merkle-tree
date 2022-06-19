85d43cde77def70940690385f5d542cc5f9822fb
Michael Mezzina <michaelmez39@gmail.com>
2022-06-18 23:14:46 -0400

    work on add push to merkle tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
 #![allow(unused)]
 use std::collections::hash_map::DefaultHasher;
+use std::collections::VecDeque;
 use std::hash::{BuildHasher, Hash, Hasher, BuildHasherDefault};
 
 type DefaultBuildHasher = BuildHasherDefault<DefaultHasher>;
 #[derive(PartialEq, Debug)]
 struct MerkleTree<T, S> {
     hasher: S,
-    head: Box<MerkleNode<T>>,
+    head: MerkleNode<T>,
+}
+
+#[derive(PartialEq, Debug)]
+struct BranchNode<T> {
+    hash: u64,
+    left: Box<MerkleNode<T>>,
+    right: Box<MerkleNode<T>>
+}
+
+#[derive(PartialEq, Debug)]
+struct LeafNode<T> {
+    hash: u64,
+    data: T
 }
 
 #[derive(PartialEq, Debug)]
 enum MerkleNode<T> {
-    Branch(u64),
-    Leaf(T),
+    Branch(BranchNode<T>),
+    Leaf(LeafNode<T>),
     Empty,
 }
 
@@ -22,16 +36,39 @@ where
 {
     fn new() -> Self {
         Self {
-            head: Box::new(MerkleNode::Empty),
+            head: MerkleNode::Empty, // may want to initialize this as a branch with two empty children?
             hasher: DefaultBuildHasher::default(),
         }
     }
 }
 
-impl<T, S> MerkleTree<T,S> {
+impl<T, S> MerkleTree<T,S>
+where S: BuildHasher, T: Hash {
     /// Adds a new data block to the merkle tree
-    fn push() {
+    fn push(&mut self, data: T) {
+        let mut queue = VecDeque::new();
+        queue.push_back(&mut self.head);
+        while let Some(mut current) = queue.pop_front() {
+            match current {
+                MerkleNode::Branch(branch) => {
+                    queue.push_back(&mut branch.left);
+                    queue.push_back(&mut branch.right);
+                }
+                MerkleNode::Leaf(_)=> (),
+                MerkleNode::Empty => {
+                    let mut hasher = self.hasher.build_hasher();
+                    data.hash(&mut hasher);
+                    let mut new_node = Box::new(MerkleNode::Leaf(LeafNode {
+                        data,
+                        hash: hasher.finish()
+                    }));
+                    std::mem::swap( current, &mut new_node);
+                    // TODO: Update the hashes on each of the other nodes
+                    return;
+                }
 
+            }
+        }
     }
 }
 
@@ -44,7 +81,7 @@ mod test {
         let tree: MerkleTree<u64, BuildHasherDefault<DefaultHasher>> = MerkleTree::new();
         assert_eq!(
             tree.head,
-            Box::new(MerkleNode::Empty)
+            MerkleNode::Empty
         );
         
     }
